5616e2fddb07153ac0c06d7af24a9bdffb79dd18
Taymur <t.ghalob@constructsys.com>
2021-11-25 16:08:57 +0000

    CSS should be functional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import { useState, useEffect, React } from 'react';
-import basket from './images/BasketIcon.png';
-import catLogo from './images/CatLogo.jpg';
-import './App.css';
-
-=======
 import { useEffect, useRef, useState } from "react";
 import React from 'react';
 import './App.css';
@@ -54,142 +47,141 @@ const App = () => {
   }, []);
 
   // Slideshow Functionality
->>>>>>> becc6833114c63cda3482fc142427d6b3f8b37d3
 
-function resetTimeout() {
-  if (timeoutRef.current) {
-    clearTimeout(timeoutRef.current);
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
   }
-}
 
-useEffect(() => {
-  resetTimeout();
-  timeoutRef.current = setTimeout(
-    () =>
-      setIndex((prevIndex) =>
-        prevIndex === cats.length - 1 ? 0 : prevIndex + 1
-      ),
-    delay
-  );
-  return () => {
+  useEffect(() => {
     resetTimeout();
-  };
-}, [index]);
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === cats.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
 
-if (error.error) {
-  return <h1>{error.message}</h1>;
-}
+  if (error.error) {
+    return <h1>{error.message}</h1>;
+  }
 
-// Page
+  // Page
 
-return (
-  <div>
-
-    {/* Nav Bar */}
-
-    <div className="nav-bar">
-      <nav className="pages">
-        <a href="#">Home</a>
-        <a href="#">Cats</a>
-        <a href="#">Checkout</a>
-        <span className="basket"><img src={basket} alt="Basket-Icon" /></span>
-      </nav>
-    </div>
-
-    {/* Title */}
-
+  return (
     <div>
-      <h1>Cats</h1>
-    </div>
 
-    {/* Slideshow */}
+      {/* Nav Bar */}
 
-    <div>
-      <div id="slideshow">
-        <div className="slideshow">
-          <div className="slideshowSlider"
-            style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
-          >
-            {cats.map((cat, index) => {
-              return (
-                <img className="slide" key={index} src={cat.url} alt="Cat" />
-              )
-            })}
-          </div>
-          <div className="slideshowDots">
-            {cats.map((_, idx) => (
-              <div
-                key={idx}
-                className={`slideshowDot${index === idx ? " active" : ""}`}
-                onClick={() => {
-                  setIndex(idx);
-                }}
-              ></div>
-            ))}
-          </div>
-        </div>
+      <div className="nav-bar">
+        <nav className="pages">
+          <a href="#">Home</a>
+          <a href="#">Cats</a>
+          <a href="#">Checkout</a>
+          <span className="basket"><img src={basket} alt="Basket-Icon" /></span>
+        </nav>
       </div>
 
-      {/* Cat Elements */}
-
-      <div className="row">
-        {cats ? (
-          <>
-            {cats.map((cat, index) => {
-              return (
-
-                // Individual Cat Card
-
-                <div className="card-space">
-                  <div>
-
-                    <div className="card">
-
-                      <p>Name goes here</p>
-                      <img src={cat.url} alt="Cat-Picture" />
-                      <p>Price goes here</p>
-
-                      <div>
-                        <button>Add to Basket</button>
-                      </div>
-                    </div>
-                  </div>
-
-                </div>
-              )
-            })}
-          </>
-
-        ) : (
-          <h1>loading cats...</h1>
-        )}
-
-      </div>
-
-      {/* Page Footer */}
+      {/* Title */}
 
       <div>
+        <h1>Cats</h1>
+      </div>
 
-        <div className="footer">
-          <div>
-            <a href="#">Home</a>
-          </div>
-          <div>
-            <a href="#">Cats</a>
-          </div>
-          <div>
-            <a href="#">Checkout</a>
-          </div>
-          <div>
-            <a href="#">Contact us</a>
+      {/* Slideshow */}
+
+      <div>
+        <div id="slideshow">
+          <div className="slideshow">
+            <div className="slideshowSlider"
+              style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+            >
+              {cats.map((cat, index) => {
+                return (
+                  <img className="slide" key={index} src={cat.url} alt="Cat" />
+                )
+              })}
+            </div>
+            <div className="slideshowDots">
+              {cats.map((_, idx) => (
+                <div
+                  key={idx}
+                  className={`slideshowDot${index === idx ? " active" : ""}`}
+                  onClick={() => {
+                    setIndex(idx);
+                  }}
+                ></div>
+              ))}
+            </div>
           </div>
         </div>
 
-      </div>
+        {/* Cat Elements */}
 
+        <div className="row">
+          {cats ? (
+            <>
+              {cats.map((cat, index) => {
+                return (
+
+                  // Individual Cat Card
+
+                  <div className="card-space">
+                    <div>
+
+                      <div className="card">
+
+                        <p>Name goes here</p>
+                        <img src={cat.url} alt="Cat-Picture" />
+                        <p>Price goes here</p>
+
+                        <div>
+                          <button>Add to Basket</button>
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
+                )
+              })}
+            </>
+
+          ) : (
+            <h1>loading cats...</h1>
+          )}
+
+        </div>
+
+        {/* Page Footer */}
+
+        <div>
+
+          <div className="footer">
+            <div>
+              <a href="#">Home</a>
+            </div>
+            <div>
+              <a href="#">Cats</a>
+            </div>
+            <div>
+              <a href="#">Checkout</a>
+            </div>
+            <div>
+              <a href="#">Contact us</a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default App;
