376aafa6d7d353a1464014763899e51db5c12350
Tati Fernandes <tatifernandes026@gmail.com>
2021-11-25 10:44:01 +0000

    api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,74 @@
+import { useEffect, useState } from "react";
 import React from 'react';
 import './App.css';
-import {price} from 'faker'
+import faker from 'faker'; 
 
-function App() {
-  return <h1>Hello World</h1>
+
+
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({
+    error: false,
+    message: "",
+  });
+  const [cats, setCats] = useState([]);
+  const handler = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10");
+      console.log(response);
+      if (response.status !== 200) {
+        throw new Error("Something got wrong");
+      }
+      
+      const data = await response.json();
     
-}
+      setCats(data);
+      console.log(data)
+      setLoading(false);
+    } catch (e) {
+      setError({ error: true, message: e.message });
+    }
+  };
+ 
+  useEffect(() => {
+    handler();
+  }, []);
+
+  if (error.error) {
+    return <h1>{error.message}</h1>;
+  }
+
+   
+  return (
+    <div>
+      <h1>Cats</h1>
+      {cats ? (
+        <>
+               {cats.map((cat, index) => {
+                return <div>
+                  <img src ={cat.url} alt =""></img>
+                </div>
+               
+            })}
+          </>
+              
+      ) : (
+        <h1>loading cats...</h1>
+      )}
+    </div>
+  );
+};
+
+
+
+
 
 export default App;
+  
+
+    
+
+
+
